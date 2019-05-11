55342b03fc5bd48075382454c12b81f7730c71f1
Mark Harrison <mhworld11@icloud.com>
2019-05-11 09:33:00 -0700

    resubmits refined 1 & 2

--- whiteboard.rb
+++ whiteboard.rb
@@ -1,24 +1,39 @@
-#1.
-array = []
-index = 0
-100.times do
-  array[index] = index + 1
-  index = index + 1
+#1. Write a method that returns an array of every number from 1 to 100.
+
+def every_number(array)
+  array = []
+  index = 0
+  100.times do
+    array[index] = index + 1
+    index = index + 1
+  end
+  p array
 end
 
-p array
+p every_number([])
 
-#2. 
-array = []
-index = 0
-counter = 1
-50.times do
-  array[index] = counter
-  index += 1
-  counter +=2
+#2. Write a method that returns an array of every other number from 1 to 100. 
+def every_other(array)
+  array = []
+  index = 0
+  counter = 1
+
+  50.times do
+    array[index] = counter
+    index += 1
+    counter +=2
+  end
+
+  p array
 end
 
-p array
+p every_other([])
+
+#3. Write a method that returns all numbers from 1 to 1000 that are divisible by 3
+
+
+
+
 
 
 
